fb14f89ee4fdca7d99d67c89a01aa327a3cfecda
SaulinhoDevs <157417082+SaulinhoDevs@users.noreply.github.com>
2025-08-27 13:25:05 -0300

    Update DB.java

--- src/src/db/DB.java
+++ src/src/db/DB.java
@@ -12,8 +12,8 @@ public class DB {
         if (conn == null) {
             try {
                 Properties props = loadProperties();
-                String url = props.getProperty();
-                conn = DriverManager.getConnection();
+                String url = props.getProperty("dburl");
+                conn = DriverManager.getConnection(url, props);
             } catch (SQLException e) {
                 throw new DbException(e.getMessage());
             }
