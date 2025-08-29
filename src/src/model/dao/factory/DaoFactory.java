85b7ba1cde6b0f0aafd8f030e3aff405b19352bd
SaulinhoDevs <157417082+SaulinhoDevs@users.noreply.github.com>
2025-08-29 17:27:31 -0300

    Add fábrica SellerDao e implementação JDBC

---
+++ src/src/model/dao/factory/DaoFactory.java
@@ -0,0 +1,12 @@
+package model.dao.factory;
+
+import model.dao.SellerDao;
+import model.dao.impl.SellerDaoJDBC;
+
+public class DaoFactory {
+
+    public static SellerDao createSellerDao() {
+        return new SellerDaoJDBC();
+    }
+
+}
