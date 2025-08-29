67422cea86cc68ab1a596dec0da06eaa681c576c
SaulinhoDevs <157417082+SaulinhoDevs@users.noreply.github.com>
2025-08-29 20:28:17 -0300

    Add método findByDepartment para SellerDao

--- src/src/application/Program.java
+++ src/src/application/Program.java
@@ -2,7 +2,10 @@ package application;
 
 import model.dao.SellerDao;
 import model.dao.factory.DaoFactory;
+import model.entities.Department;
 import model.entities.Seller;
+
+import java.util.List;
 
 public class Program {
     public static void main(String[] args) {
@@ -13,5 +16,11 @@ public class Program {
 
         System.out.println("Teste 1 -> Busca por ID");
         System.out.println(seller);
+
+        System.out.println("\nTeste 2 -> Busca por Departamento");
+        List<Seller> list = sellerDao.findByDepartment(new Department(2, null));
+        for (Seller obj : list) {
+            System.out.println(obj);
+        }
     }
 }
