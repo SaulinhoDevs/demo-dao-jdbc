7a37efbbdb1658c230d9f161ec8a5777ca1bbbde
SaulinhoDevs <157417082+SaulinhoDevs@users.noreply.github.com>
2025-08-29 18:33:44 -0300

    Implementar SellerDaoJDBC.findById e atualizar factory

--- src/src/application/Program.java
+++ src/src/application/Program.java
@@ -1,7 +1,17 @@
 package application;
 
+import model.dao.SellerDao;
+import model.dao.factory.DaoFactory;
+import model.entities.Seller;
+
 public class Program {
     public static void main(String[] args) {
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.findById(3);
+
+        System.out.println(seller);
 
     }
 }
